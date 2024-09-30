eb7cc41edfb37890f81341492e3d77f4016b73bb
Daniel Andrade <daniel.andrade63@gmail.com>
2024-09-30 09:48:58 -0300

    Configuração inicial CI/CD

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,13 @@
 Cypress.Commands.add('login', (email, senha) => {
-    cy.visit('/login')
-    cy.get('[data-test="inputLoginEmail"]').type(email)
-    cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false })
-    cy.get('[data-test="botaoTeste"]').should('be.visible').click()
+    cy.session([email, senha], () => {
 
+        cy.visit('/login');
+        cy.get('[data-test="inputLoginEmail"]').type(email);
+        cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false });
+        cy.get('[data-test="botaoTeste"]').should('be.visible').click();
+        cy.location('pathname').should('eq', '/dashboard');
+    })
 })
-
 
 Cypress.Commands.add('cadastraEspecialista', (nome, email, senha, especialidade, crm, imagem, cep, rua, numero, complemento, estado) => {
     cy.visit('/dashboard')
@@ -23,4 +25,22 @@ Cypress.Commands.add('cadastraEspecialista', (nome, email, senha, especialidade,
     cy.get('[data-test="inputEspecialistaComplemento"]').type(complemento)
     cy.get('[data-test="inputEspecialistaEstado"]').type(estado)
 
+})
+
+Cypress.Commands.add('loginApi', (email, senha) => {
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('api_login'),
+        body: {
+            email: email,
+            senha: senha
+        },
+        failOnStatusCode: false
+    }).then(response => {
+        expect(response.status).to.eq(200);
+        expect(response.body.auth).to.be.true;
+        expect(response.body.rota).to.eq('/clinica');
+        expect(response.body.token).to.exist;
+        cy.wrap(response.body.token).as('token');
+    })
 })
